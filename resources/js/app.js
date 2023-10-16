08bc92a0584d157cbdb1864c466a3918bab28a50
maxpietersma <max.pietersma@gmail.com>
2023-10-16 12:38:08 +0200

    installed vue-i18n

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,5 +1,8 @@
 import { createApp, h } from "vue"
 import { createInertiaApp } from "@inertiajs/inertia-vue3"
+import {createI18n} from "vue-i18n";
+import en_lang from "../lang/en.json";
+import nl_lang from "../lang/nl.json";
 
 createInertiaApp({
 	resolve: name => {
@@ -7,9 +10,20 @@ createInertiaApp({
 		return pages[`./Pages/${name}.vue`]
 	},
 	setup({ el, App, props, plugin }) {
+		const i18n = createI18n({
+			locale: "en",
+			fallbackLocale: "nl",
+			globalInjection: true,
+			messages: {
+				en: en_lang,
+				nl: nl_lang,
+			},
+		})
 		createApp({ render: () => h(App, props) })
 			.use(plugin)
+			.use(i18n)
 			.mount(el)
+
 	},
 })
 
