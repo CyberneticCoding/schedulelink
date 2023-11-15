8e4c93e7d7677d32540e5b6fa24d6aa2703d89a2
maxpietersma <max.pietersma@gmail.com>
2023-11-15 20:49:45 +0100

    changed calendar block size

--- resources/js/Components/CalendarGrid.vue
+++ resources/js/Components/CalendarGrid.vue
@@ -110,106 +110,106 @@
 					<div class="sticky left-0 z-10 w-10 sm:w-14 flex-none bg-white ring-1 ring-gray-100" />
 					<div class="grid flex-auto grid-cols-1 grid-rows-1">
 						<!-- Horizontal lines -->
-						<div class="col-start-1 col-end-2 row-start-1 grid divide-y divide-gray-100 grid-rows-[repeat(24,minmax(2rem,1fr))] sm:grid-rows-[repeat(48,minmax(2rem,1fr))]">
+						<div class="col-start-1 col-end-2 row-start-1 grid divide-y divide-gray-100 ggrid-rows-[repeat(24,minmax(2rem,1fr))] grid-rows-[repeat(24,minmax(2rem,1fr))]">
 							<div>
 								<div class="sticky left-0 z-20 -ml-14 mt-0.5 w-10 sm:w-14 pr-2 text-right text-xs leading-5 text-gray-400">12AM</div>
 							</div>
-							<div class="hidden sm:inline"/>
+							<!--<div class="hidden sm:inline"/>-->
 							<div>
 								<div class="sticky left-0 z-20 -ml-14 -mt-2.5 w-10 sm:w-14 pr-2 text-right text-xs leading-5 text-gray-400">1AM</div>
 							</div>
-							<div class="hidden sm:inline"/>
+							<!--<div class="hidden sm:inline"/>-->
 							<div>
 								<div class="sticky left-0 z-20 -ml-14 -mt-2.5 w-10 sm:w-14 pr-2 text-right text-xs leading-5 text-gray-400">2AM</div>
 							</div>
-							<div class="hidden sm:inline"/>
+							<!--<div class="hidden sm:inline"/>-->
 							<div>
 								<div class="sticky left-0 z-20 -ml-14 -mt-2.5 w-10 sm:w-14 pr-2 text-right text-xs leading-5 text-gray-400">3AM</div>
 							</div>
-							<div class="hidden sm:inline"/>
+							<!--<div class="hidden sm:inline"/>-->
 							<div>
 								<div class="sticky left-0 z-20 -ml-14 -mt-2.5 w-10 sm:w-14 pr-2 text-right text-xs leading-5 text-gray-400">4AM</div>
 							</div>
-							<div class="hidden sm:inline"/>
+							<!--<div class="hidden sm:inline"/>-->
 							<div>
 								<div class="sticky left-0 z-20 -ml-14 -mt-2.5 w-10 sm:w-14 pr-2 text-right text-xs leading-5 text-gray-400">5AM</div>
 							</div>
-							<div class="hidden sm:inline"/>
+							<!--<div class="hidden sm:inline"/>-->
 							<div>
 								<div class="sticky left-0 z-20 -ml-14 -mt-2.5 w-10 sm:w-14 pr-2 text-right text-xs leading-5 text-gray-400">6AM</div>
 							</div>
-							<div class="hidden sm:inline"/>
+							<!--<div class="hidden sm:inline"/>-->
 							<div>
 								<div class="sticky left-0 z-20 -ml-14 -mt-2.5 w-10 sm:w-14 pr-2 text-right text-xs leading-5 text-gray-400">7AM</div>
 							</div>
-							<div class="hidden sm:inline"/>
+							<!--<div class="hidden sm:inline"/>-->
 							<div>
 								<div class="sticky left-0 z-20 -ml-14 -mt-2.5 w-10 sm:w-14 pr-2 text-right text-xs leading-5 text-gray-400">8AM</div>
 							</div>
-							<div class="hidden sm:inline"/>
+							<!--<div class="hidden sm:inline"/>-->
 							<div>
 								<div class="sticky left-0 z-20 -ml-14 -mt-2.5 w-10 sm:w-14 pr-2 text-right text-xs leading-5 text-gray-400">9AM</div>
 							</div>
-							<div class="hidden sm:inline"/>
+							<!--<div class="hidden sm:inline"/>-->
 							<div>
 								<div class="sticky left-0 z-20 -ml-14 -mt-2.5 w-10 sm:w-14 pr-2 text-right text-xs leading-5 text-gray-400">10AM</div>
 							</div>
-							<div class="hidden sm:inline"/>
+							<!--<div class="hidden sm:inline"/>-->
 							<div>
 								<div class="sticky left-0 z-20 -ml-14 -mt-2.5 w-10 sm:w-14 pr-2 text-right text-xs leading-5 text-gray-400">11AM</div>
 							</div>
-							<div class="hidden sm:inline"/>
+							<!--<div class="hidden sm:inline"/>-->
 							<div>
 								<div class="sticky left-0 z-20 -ml-14 -mt-2.5 w-10 sm:w-14 pr-2 text-right text-xs leading-5 text-gray-400">12PM</div>
 							</div>
-							<div class="hidden sm:inline"/>
+							<!--<div class="hidden sm:inline"/>-->
 							<div>
 								<div class="sticky left-0 z-20 -ml-14 -mt-2.5 w-10 sm:w-14 pr-2 text-right text-xs leading-5 text-gray-400">1PM</div>
 							</div>
-							<div class="hidden sm:inline"/>
+							<!--<div class="hidden sm:inline"/>-->
 							<div>
 								<div class="sticky left-0 z-20 -ml-14 -mt-2.5 w-10 sm:w-14 pr-2 text-right text-xs leading-5 text-gray-400">2PM</div>
 							</div>
-							<div class="hidden sm:inline"/>
+							<!--<div class="hidden sm:inline"/>-->
 							<div>
 								<div class="sticky left-0 z-20 -ml-14 -mt-2.5 w-10 sm:w-14 pr-2 text-right text-xs leading-5 text-gray-400">3PM</div>
 							</div>
-							<div class="hidden sm:inline"/>
+							<!--<div class="hidden sm:inline"/>-->
 							<div>
 								<div class="sticky left-0 z-20 -ml-14 -mt-2.5 w-10 sm:w-14 pr-2 text-right text-xs leading-5 text-gray-400">4PM</div>
 							</div>
-							<div class="hidden sm:inline"/>
+							<!--<div class="hidden sm:inline"/>-->
 							<div>
 								<div class="sticky left-0 z-20 -ml-14 -mt-2.5 w-10 sm:w-14 pr-2 text-right text-xs leading-5 text-gray-400">5PM</div>
 							</div>
-							<div class="hidden sm:inline"/>
+							<!--<div class="hidden sm:inline"/>-->
 							<div>
 								<div class="sticky left-0 z-20 -ml-14 -mt-2.5 w-10 sm:w-14 pr-2 text-right text-xs leading-5 text-gray-400">6PM</div>
 							</div>
-							<div class="hidden sm:inline"/>
+							<!--<div class="hidden sm:inline"/>-->
 							<div>
 								<div class="sticky left-0 z-20 -ml-14 -mt-2.5 w-10 sm:w-14 pr-2 text-right text-xs leading-5 text-gray-400">7PM</div>
 							</div>
-							<div class="hidden sm:inline"/>
+							<!--<div class="hidden sm:inline"/>-->
 							<div>
 								<div class="sticky left-0 z-20 -ml-14 -mt-2.5 w-10 sm:w-14 pr-2 text-right text-xs leading-5 text-gray-400">8PM</div>
 							</div>
-							<div class="hidden sm:inline"/>
+							<!--<div class="hidden sm:inline"/>-->
 							<div>
 								<div class="sticky left-0 z-20 -ml-14 -mt-2.5 w-10 sm:w-14 pr-2 text-right text-xs leading-5 text-gray-400">9PM</div>
 							</div>
-							<div class="hidden sm:inline"/>
+							<!--<div class="hidden sm:inline"/>-->
 							<div>
 								<div class="sticky left-0 z-20 -ml-14 -mt-2.5 w-10 sm:w-14 pr-2 text-right text-xs leading-5 text-gray-400">10PM</div>
 							</div>
-							<div class="hidden sm:inline"/>
+							<!--<div class="hidden sm:inline"/>-->
 							<div>
 								<div class="sticky left-0 z-20 -ml-14 -mt-2.5 w-10 sm:w-14 pr-2 text-right text-xs leading-5 text-gray-400">11PM</div>
 							</div>
-							<div class="hidden sm:inline"/>
+							<!--<div class="hidden sm:inline"/>-->
 						</div>
 						<!-- Vertical lines -->
-						<div class="col-start-0 col-end-2 row-start-1 grid-cols-7 grid-rows-1 divide-x divide-gray-100 grid sm:grid-cols-7">
+						<div class="col-start-0 col-end-2 row-start-1 grid-cols-7 grid-rows-1 divide-x divide-gray-100 grid">
 							<div class="col-start-1 row-span-full" />
 							<div class="col-start-2 row-span-full" />
 							<div class="col-start-3 row-span-full" />
@@ -220,7 +220,7 @@
 							<!--<div class="col-start-8 row-span-full w-8" />-->
 						</div>
 						<!-- Events -->
-						<ol @click="handleGridClick" ref="calendar" class="col-start-1 col-end-2 row-start-1 grid divide-y divide-gray-100 grid-cols-7 grid-rows-[repeat(24,minmax(2rem,1fr))] sm:grid-rows-[repeat(48,minmax(2rem,1fr))]">
+						<ol @click="handleGridClick" ref="calendar" class="col-start-1 col-end-2 row-start-1 grid divide-y divide-gray-100 grid-cols-7 ggrid-rows-[repeat(24,minmax(2rem,1fr))] grid-rows-[repeat(48,minmax(1.5rem,1fr))]">
 							<TimeBlock
 								v-for="timeBlock in timeBlocks"
 								:key="timeBlock.timeblock.id"
