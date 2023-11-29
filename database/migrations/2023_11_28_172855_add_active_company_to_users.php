3a2eeb7785db955f785420809b0666b6d6859232
maxpietersma <max.pietersma@gmail.com>
2023-11-29 10:49:54 +0100

    fix dusk test

--- database/migrations/2023_11_28_172855_add_active_company_to_users.php
+++ database/migrations/2023_11_28_172855_add_active_company_to_users.php
@@ -23,8 +23,8 @@ return new class extends Migration
     public function down(): void
     {
 		Schema::table('users', function (Blueprint $table) {
-			$table->dropColumn('active_company_id');
 			$table->dropForeign(['active_company_id']);
+			$table->dropColumn('active_company_id');
 		});
     }
 };
