64ed58fac4871318634b61a97b67a0ee713b1b60
maxpietersma <max.pietersma@gmail.com>
2023-11-29 12:47:21 +0100

    icons

--- resources/js/Components/TimeBlockEdit.vue
+++ resources/js/Components/TimeBlockEdit.vue
@@ -45,12 +45,14 @@
 									type="button"
 									class="inline-flex justify-center rounded-md border border-transparent bg-blue-100 px-4 py-2 text-sm font-medium text-blue-900 hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
 									@click="closeModal">
+									<i class="fa-solid fa-xmark translate-y-0.5 mr-2"></i>
 									Close
 								</button>
 								<button
 									type="button"
 									class="inline-flex justify-center rounded-md border border-transparent bg-red-500 px-4 py-2 text-sm font-medium text-white hover:bg-red-400 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
 									@click="deleteTimeBlock">
+									<i class="fa-solid fa-trash translate-y-0.5 mr-2"></i>
 									Delete
 								</button>
 							</div>
