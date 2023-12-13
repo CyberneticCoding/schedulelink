498504198c57f3c046258cefa964368bcb475a1a
TOKEN-NL <bergje@hotmail.nl>
2023-12-13 14:11:45 +0100

    - bugfix    - get route for add company

--- routes/web.php
+++ routes/web.php
@@ -53,6 +53,7 @@ Route::group(['middleware' => ['auth']], function () {
 	Route::get('/settings/company', [SettingController::class, 'companyDetailsPage'])->name('settings.details');
 	Route::get('/settings/company/members', [SettingController::class, 'companyMembersPage'])->name('companies.members');
 
+	Route::get('/settings/company/add', [SettingController::class, 'companyAddPage'])->name('companies.add');
 	Route::post('/settings/company/add', [SettingController::class, 'addCompany']);
 	Route::group(['middleware' => ['checkCompanyOwnership']], function () {
 		Route::get('/settings/company/members/add', [SettingController::class, 'companyMembersAddPage'])->name('companies.members.add');
